05750c50c9ee2a3f55e2828fdb3cb6fbe2158890
Alex Vanston <alex@mrvdot.com>
2013-02-13 17:47:44 -0700

    Adding source validation

--- README.md
+++ README.md
@@ -14,7 +14,4 @@ Where the 'app' is your data object and 'logo_url' is the property containing th
 
 This will generate a field containing an image preview (if the src is already populated), an 'Upload images' button, and a standard text field for inputting your own url. Please note, if the src is initially populated, the upload and url fields will be hidden. Click on the image to change it, or click the 'X' in the top right corner to clear it.
 
-Known Issues
-============
-* If the src field is populated, but the image can't be loaded, Chrome just displays an empty box (known bug in Chrome that doesn't handle 'alt' text properly)
-* 'Clear' button doens't come properly styled (extracting those styles from the original app, that's coming soon)
+After initialized (or if the src value changes), EIF will attempt to validate that the source image can be loaded. If it fails to load, it will display a warning and offer the ability to clear the src and try again.
--- js/editable-image-field.js
+++ js/editable-image-field.js
@@ -1,17 +1,22 @@
 angular.module('editable-image-field',['mvd-uploadify'])
-  .directive('editableImageField',[function() {
-    var tpl = '<div class="preview">' + 
+  .directive('editableImageField',['$http','$timeout',function($http, $timeout) {
+    var tpl = '<div ng-hide="loadFailed"><div class="preview">' + 
       '<span class="clear" title="Clear image" ng-click="clearImage()">&times;</span>'+
       '<img title="Click to change" ng-src="{{src}}" alt="It appears that your selected image is not loading, click here to update it" /></div>' + 
       '<div class="set-src"><span uploadify uploadify-directory="uploadify/" mvd-options="{ onUploadSuccess : \'updateSrc\'}" ></span>' + 
-      '<label>Or set url:</label><input ng-model="src" type="text"/></div>';
+      '<label>Or set url:</label><input ng-model="src" type="text"/></div></div>' + 
+      '<div ng-show="loadFailed">' + 
+        '<p class="alert">The image you\'ve specified doesn\'t appear to be loading successfully, click <a ng-click="clearImage()">here</a> to clear it and try a new image.<span ng-show="retrying"><br />Retrying image...</span></p>' + 
+      '</div>';
     return {
       template : tpl,
       scope : {
         src : '=src'
       },
       link : function($scope,$el,$attrs) {
+        $scope.loadFailed = false;
         $el.addClass('editable-image-field');
+
         var $setSrc = $el.find('.set-src');
         var $preview = $el.find('.preview').on('click',function() {
           $preview.slideUp();
@@ -23,15 +28,47 @@ angular.module('editable-image-field',['mvd-uploadify'])
         } else {
           $preview.hide();
         }
+
+        var checkCount = 0;
+
+        var checkThreshold = $attrs.retryLimit || 3;
+
+        var validateSrc = $attrs.validateSrc || true;
+        var validationTimeout;
+
         $scope.$watch('src',function(nv,ov) {
-          if(nv && !ov) {
+          $scope.loadFailed = false;
+          checkCount = 0;
+          if(nv) {
             $setSrc.hide();
             $preview.show();
-          } else if(!nv && ov) {
+          } else {
             $preview.hide();
             $setSrc.show();
           }
+          if(nv && validateSrc) {
+            validationTimeout = $timeout(function() {
+              validateExists(nv);
+            },1000);
+          }
         });
+
+
+        var validateExists = function(src) {
+          checkCount++;
+          var img = jQuery('<img src="' + src + '" style="position:relative; left:-9999px; padding: 0; margin : 0;" />').appendTo('body');
+          $scope.loadFailed = !((img.height() > 0) && (img.width() > 0));
+          img.remove();
+          if($scope.loadFailed && checkCount < checkThreshold) {
+            $scope.retrying = true;
+            validationTimeout = $timeout(function() {
+              validateExists(src);
+            },2000);
+          } else {
+            $scope.retrying = false;
+          }
+        }
+
         $scope.updateSrc = function(file, src) {
           $scope.$apply(function() {
             $scope.src = src;  
@@ -41,6 +78,9 @@ angular.module('editable-image-field',['mvd-uploadify'])
         };
 
         $scope.clearImage = function() {
+          if(validationTimeout) {
+            $timeout.cancel(validationTimeout);
+          }
           $scope.src = null;
         }
       }
@@ -65,6 +105,7 @@ angular.module('mvd-uploadify',[])
         };
 
         var onUploadSuccess = function(file,response) {
+          if(response == 'UPLOAD_FAILED') return;//trigger onUploadError
           if(userUploadSuccess && typeof($scope[userUploadSuccess]) === 'function') {
             $scope[userUploadSuccess](file,response);
           }
